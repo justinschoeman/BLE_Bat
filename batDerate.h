30a80c0ff7f577591a222f2a7b52071de20fbcab
Justin Schoeman <justin.schoeman@gmail.com>
2023-02-22 22:31:21 +0200

    finished but untested derate

--- batDerate.h
+++ batDerate.h
@@ -10,7 +10,8 @@ public:
     lastMillis(0),
     bulk(true),
     bulkMillis(0),    
-    derateErrorCount(0)
+    derateErrorCount(0),
+    lvLockout(false)
     {}
   
   void run(void) {
@@ -57,107 +58,72 @@ public:
         bulkMillis = millis();
       }
     }
-  }
 
-/*
+    // check charge derating
+    if(bat->maxCellVoltage > cfgCellMaxV) {
+      // overvoltage - shut off charging...
+      Serial.println("OVERVOLTAGE!");
+      setChargeI(0.0f);
+      setChargeV(bat->voltage); // go no higher
+    } else {
+      // not overvoltage - set sane target voltage
+      // always start with some sane defaults
+      // for sanity sake, always keep the lowest meaningful charge voltage, and ramp up as required...
+      float t = cfgCellMaxV - bat->maxCellVoltage;
+      t *= (float)bat->numCells;  // set the target voltage = current voltage + whatever is required to make the max cell full (spread over all cells)
+      // inverter is too gentle close to voltage limits - lets make it slightly more aggressive
+      t *= 2.0f;
+      setChargeV(bat->voltage + t);
 
-void derate(void) {
-  uint16_t delta;
-  uint32_t t32;
-
-  // always start with some sane defaults
-  // for sanity sake, always keep the lowest meaningful charge voltage, and ramp up as required...
-  // this  could potentially overflow
-  t32 = CELL_MAX_V - bat_maxv;
-  t32 *= (uint32_t)CELL_CNT;  // set the target voltage = current voltage + whatever is required to make the max cell full (spread over all cells)
-  // inverter is too gentle close to voltage limits - lets make it slightly more aggressive
-  t32 *= (uint32_t)2;
-  derate_set_trg_v(t32);
-  
-  // default to normal max current
-  trg_chg_i = BAT_CHG_I*100;
-
-  // HIGH VOLTAGE LOGIC
-  if(bat_maxv > CELL_MAX_V) {
-    // over - shut off charging...
-    Serial.println("OVERVOLTAGE!");
-    trg_chg_i = 0;
-    derate_set_trg_v(0);
-    // do not do this - implausible values can cause inverter to ignore us!
-    //trg_chg_v -= (bat_maxv - BAT_MAX_V) * 200; // retard by 200mV for each mV above target...
-  } else if(bat_maxv > CELL_DERATE_V) {
-    Serial.println("HIGH VOLTAGE!");
-    // derate voltage - done globally above as the default
-    t32 = CELL_MAX_V - bat_maxv;
-    t32 *= trg_chg_i;
-    t32 /= (uint32_t)(CELL_MAX_V - CELL_DERATE_V); // can never remeber C rules - just make 100% sure this doesn't demote somewhere
-    trg_chg_i = t32;
-  }
-  
-  // test - hard limit current in closing phases...
-  if(bat_maxv > 3500) trg_chg_i = trg_chg_i / 10; // max 15A
-
-  // ramp functions
-  if(trg_chg_i < bat_chg_i) {
-    // going down? rapid derate
-    delta = bat_chg_i - trg_chg_i;
-    delta /= 2;
-    if(delta == 0) delta = 1;
-    bat_chg_i -= delta;
-  } else if(trg_chg_i > bat_chg_i) {
-    // going up? very slow
-    delta = trg_chg_i / 100; //100s ramp up total
-    if(delta < 1) delta = 1;
-    bat_chg_i += delta;
-    if(bat_chg_i > trg_chg_i) bat_chg_i = trg_chg_i;
-  }
-
-  if(trg_chg_v < bat_chg_v) {
-    // going down? rapid derate
-    delta = bat_chg_v - trg_chg_v;
-    delta /= 2;
-    if(delta == 0) delta = 1;
-    bat_chg_v -= delta;
-  } else if(trg_chg_v > bat_chg_v) {
-    // going up? very slow
-    bat_chg_v += 10; // 100 s per volt... 10s per unit that the inverter can notice
-    if(bat_chg_v > trg_chg_v) bat_chg_v = trg_chg_v;
-  }
-  Serial.print("bat_chg_v: ");
-  Serial.println(bat_chg_v);
-  Serial.print("trg_chg_v: ");
-  Serial.println(trg_chg_v);
-  Serial.print("bat_chg_i: ");
-  Serial.println(bat_chg_i);
-  Serial.print("trg_chg_i: ");
-  Serial.println(trg_chg_i);
-
-  // LOW VOLTAGE LOGIC
-
-  // default to normal maximum
-  bat_dis_i = BAT_DIS_I * 100U;
-  
-  if(bat_minv <= CELL_MIN_V) {
-    // any cell <= minimum? shut off discharge
-    Serial.println("LOW VOLTAGE LIMIT!");
-    bat_dis_i = 0;
-    bat_soc = 0;
-    bat_soh = 0;
-    lv_lockout = 1;
-    lv_lockout_ms = millis();
-  } else {
-    // otherwise wait for lockout interval then release
-    if(lv_lockout) {
-      Serial.println("LOW VOLTAGE LOCKOUT");
-      bat_dis_i = 0;
-      bat_soc = 0;
-      bat_soh = 0;
-      if(millis() - lv_lockout_ms >= LV_LOCKOUT_MS) {
-        lv_lockout = 0;        
+      // and set current based on derate curve
+      if(bat->maxCellVoltage > cfgCellMaxDerateV) {
+        // derating
+        Serial.println("derating!");
+        float t = cfgCellMaxV - bat->maxCellVoltage;
+        t /= cfgCellMaxV - cfgCellMaxDerateV; // proportional over derate range
+        setChargeI(bat->nomChargeCurrent * t);
+        // this is an extra gotcha from my old production code
+        // above some arbitrary hard limit, drastically reduce current...
+        // test - hard limit current in closing phases...
+        if(bat->maxCellVoltage > 3.5f) bat->chargeCurrent /= 10.0f; // max 15A
+      } else {
+        // no derate - default to nominal
+        setChargeI(bat->nomChargeCurrent);
       }
     }
+
+    // check discharge derating
+    if(bat->minCellVoltage < cfgCellMinV) {
+      // any cell <= minimum? shut off discharge
+      Serial.println("LOW VOLTAGE LIMIT!");
+      lvLockout = true;
+      lvLockoutMillis = millis();
+    }
+    // otherwise, make sure any low voltage lockout has expired
+    if(lvLockout) {
+      Serial.println("LOW VOLTAGE LOCKOUT");
+      bat->dischargeCurrent = 0.0f;
+      bat->soc = 0.0f;
+      if(millis() - lvLockoutMillis >= cfgLvLockoutMillis) {
+        lvLockout = false;
+      }
+    } else {
+      // default to nominal discharge
+      bat->dischargeCurrent = bat->nomDischargeCurrent;
+      if(bat->minCellVoltage < cfgCellMinDerateV) {
+        // derate soc
+        float t = bat->minCellVoltage - cfgCellMinV;
+        float t2 = cfgCellMinDerateV - cfgCellMinV;
+        t = (t * t) / (t2 * t2);
+        t *= 100.0f;
+        if(t < bat->soc) {
+          Serial.println("LOW VOLTAGE DERATE SOC");
+          bat->soc = t;
+        }
+      }
+    }
+    //bat->dump();
   }
-  */
 
 private:
 
@@ -166,7 +132,7 @@ private:
   // in this version, let's make sure all cells are nearly full and well balanced before going to float.
   // FIXME - do this once every X cycles
   bool testFloat(void) {
-    if(bat->minCellVoltage < cfgCellFloatV) return false; // at least one cell is too low
+    if(bat->minCellVoltage < cfgCellFloatStartV) return false; // at least one cell is too low
     if(bat->current > 0.0f) return false; // discharging
     if(-bat->current > bat->nomChargeCurrent * 0.03f) return false; // discharging at more than 3% of nom
     Serial.println("FLOAT OK");
@@ -200,12 +166,15 @@ private:
   bool bulk; // bulk/float mode
   unsigned long bulkMillis;
   int derateErrorCount;
+  bool lvLockout;
+  unsigned long lvLockoutMillis;
 
   // fixme - config should be configurable...
   const float cfgCellMaxV = 3.65f; // maximum cell voltage
+  const float cfgCellMaxDerateV = cfgCellMaxV - 0.2f; // maximum cell voltage before we derate
   const float cfgCellMinV = 2.5f; // minimum cell voltage
-  const float cfgCellDerateV = cfgCellMaxV - 0.2f; // maximum cell voltage before we derate
-  const float cfgCellFloatV = 3.6f; // target cell voltage in float mode
+  const float cfgCellMinDerateV = cfgCellMinV + 0.2f; // minimum cell voltage before we derate
+  const float cfgCellFloatStartV = 3.6f; // target cell voltage in float mode
   const float cfgCellFloatTargetV = 3.4f; // target charge voltage in float mode
   const float cfgCellFloatEndV = 3.35f; // end float before this voltage
   const unsigned long cfgLvLockoutMillis = (5UL * 60UL * 1000UL); // min low voltage recovery time before we restore dischare caps
